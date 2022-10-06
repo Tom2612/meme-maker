d6572cd27687eef613bab2090102b0042a2a81dc
Tom <tomcat-26@hotmail.co.uk>
2022-10-06 03:06:11 +0100

    set up project

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Main () {
+    return(
+        <main>
+            <h2>Main here</h2>
+        </main>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Navbar () {
+    return(
+        <header className="header">
+            <img src='../images/troll-face.png' className="header--image"/>
+            <h2>Navbar here</h2>
+        </header>
+    )
+}
