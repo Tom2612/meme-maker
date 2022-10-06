2383d4e5a4299ffd46c368ef1f23e3d3dccd2552
Tom <tomcat-26@hotmail.co.uk>
2022-10-06 04:40:32 +0100

    Added inputs and button to Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,28 @@ import React from 'react';
 export default function Main () {
     return(
         <main>
-            <h2>Main here</h2>
+            <div className="form">
+                <input 
+                    type='text'
+                    placeholder='Top text'
+                    className="form--input"
+                />
+                <input 
+                    type='text'
+                    placeholder='Bottom text'
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                >
+                    Get new meme image
+                </button>
+            </div>
+            <div>
+                <img src="http://i.imgflip.com/1bij.jpg"/>
+                <h2 className="meme--text top"></h2>
+                <h2 className="meme--text bottom"></h2>
+            </div>
         </main>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Navbar () {
     return(
         <header className="header">
-            <img src='../images/troll-face2.png' className="header--image"/>
+            <img src='https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png' className="header--image"/>
             <h2 className="header--title">MemeMaker</h2>
         </header>
     )
